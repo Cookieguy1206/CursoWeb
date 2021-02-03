c4bf8ecbabd20df05b80ddff4e1724e49073006e
Cookie <iiklygamer89@gmail.com>
2021-02-03 14:49:30 -0500

    Avanzando con Async / Await

--- IntroduccionJS/JS/26.js
+++ IntroduccionJS/JS/26.js
@@ -3,9 +3,25 @@ let boton = document.getElementById("boton");
 
 //AÑADIR UN OYENTE DE EVENTOS 
 //SE LE TIENEN QUE PASAR DOS PARAMETROS:
+//SE LE TIENEN QUE PASAR DOS PARAMETROS:
+//SE LE TIENEN QUE PASAR DOS PARAMETROS:c
 //EL PRIMERO ES EL EVENTO
 //EL SEGUNDO ES UNA FUNCION
-
 boton.addEventListener("click", click = () => {
-    console.log("Diste click");
+    //PREGUNTAR SI QUIERE RECIBIR NOTIFICACIONES
+    Notification.requestPermission()
+    .then((result) => {
+        console.log("El resultado es: " + result);
+    }).catch((err) => {
+        console.log("El resultado es: " + err);
+    });
+
+    if (Notification.permission == "granted") {
+        //CREAMOS LA NOTIFICACION
+        //RECIBE DOIS PARAMETROS: EL TITULO Y UN OBJETO CON LA IMAGEN Y EL CUERPO DE LA NOTIFICACION
+        new Notification("Esta es una notificacion", {
+            icon: "img/icon.png",
+            body: "Hola guapo"
+        });
+    }
 });
