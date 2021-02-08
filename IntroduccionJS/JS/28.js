e1eddffb38bea10d737eeb49b222c34dad1d6c5f
Cookie <iiklygamer89@gmail.com>
2021-02-08 16:39:40 -0500

    Fecth API terminado

--- IntroduccionJS/JS/28.js
+++ IntroduccionJS/JS/28.js
@@ -1,14 +1,21 @@
 //FETCH API
 //PERMITE ENVIAR INFORMACION AL SERVIDOR U OBTELERLA
 //TAMBIEN PERMITE ACTUALIZAR LA APP SIN NECRSIDAD DE RECARGAR LA PAGINA
-let archivo = "empleados.json"
+let archivo = "empleados.json";
 
 let obtenerEmpleados = () => {
+    //fetch() RECIBE COMO PARAMETRO UNA URL
     fetch(archivo)
     .then((result) => {
-        console.log(result);
-    }).catch((err) => {
-        console.log(err);
+        return result.json();
+    })
+    .then(datos => {
+        const {empleados} = datos;
+        console.log(empleados);
+
+        empleados.forEach(empleado => {
+            console.log(empleado);
+        });
     });
 }
 
